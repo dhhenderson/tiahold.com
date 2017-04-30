6d6bb630ab6ee48105dcc54a897f603b1cfa893f
Drake Henderson <drakehenderson@yahoo.com>
2017-04-29 23:10:52 -0400

    Add CodeStar web service

--- tiahold.py
+++ tiahold.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, json
 from flask_bootstrap import Bootstrap
+import requests
 
 app = Flask(__name__)
 Bootstrap(app)
+
+# TODO: lambda throttling, notifications on errors
+# TODO: make url a config setting; does it change? if so, how to update? dynamoDB? tests
+
+@app.route('/_get_timestamp')
+def get_timestamp():
+    try:
+        r = requests.get('https://d7xrbctt1l.execute-api.us-east-1.amazonaws.com/Prod/', timeout=2)
+        data = json.loads(r.text)
+        app.logger.info(r.text)
+        return data['timestamp']
+    except requests.exceptions.RequestException as e:
+        app.logger.error(e)
+        return 'Timestamp unavailable'
 
 @app.route('/')
 @app.route('/index.html')
@@ -13,4 +28,3 @@ def index():
 # We only need this for local development.
 if __name__ == '__main__':
     app.run()
-
